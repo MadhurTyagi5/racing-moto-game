adb81080796f19d5798425f6bebfd4846e418cfa
Vishwajeet-coder07 <vishwajeetrajput282@gmail.com>
2025-09-22 18:28:47 +0530

    moving_car

--- app.js
+++ app.js
@@ -4,14 +4,46 @@ const ctx = canvas.getContext("2d");
 canvas.width = 500;
 canvas.height = window.innerHeight;
 
+
+let imageX = canvas.width / 2 - 25;
+let imageY = canvas.height - 100;
+const imageWidth = 50;
+const imageHeight = 100;
+const movespeed = 10;
+
 const img = new Image();
 img.src = "road.jpg";
-img.onload = function () {
-    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
- }   
 
 const moto = new Image();
 moto.src = "car2.png";
-moto.onload = function () {
-    ctx.drawImage(moto, canvas.width / 2 - 25, canvas.height - 100, 50, 100);
-};
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+
+    if (img.complete) {
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    }
+
+
+    if (moto.complete) {
+        ctx.drawImage(moto, imageX, imageY, imageWidth, imageHeight);
+    }
+}
+
+function update() {
+    draw();
+    requestAnimationFrame(update);
+}
+document.addEventListener("keydown", (e)=> {
+    if(e.key === "ArrowLeft" && imageX > 50){
+        imageX -= movespeed;
+    }
+    if(e.key === "ArrowRight" && imageX < 454 - imageWidth){
+        imageX += movespeed;
+    }
+});
+update();
+
